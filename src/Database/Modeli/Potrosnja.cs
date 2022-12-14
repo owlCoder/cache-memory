338c8b3be2cfd792f2e1c2e84cb80297b1f9f20c
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-14 21:47:19 +0100

    Potrosnja: Add Constructor

--- src/Database/Modeli/Potrosnja.cs
+++ src/Database/Modeli/Potrosnja.cs
@@ -1,11 +1,24 @@
-﻿namespace Database.Modeli
+﻿using System;
+
+namespace Database.Modeli
 {
     public class Potrosnja
     {
+        public Potrosnja(int userId, int brojiloId, int mesec, string grad, double zabelezenaPotrosnja)
+        {
+            UserId = userId;
+            BrojiloId = brojiloId;
+            Mesec = mesec;
+            Grad = grad ?? throw new ArgumentNullException(nameof(grad));
+            ZabelezenaPotrosnja = zabelezenaPotrosnja;
+        }
+
         public int UserId { get; set; }
         public int BrojiloId { get; set; }
         public int Mesec { get; set; }
         public string Grad { get; set; }
         public double ZabelezenaPotrosnja { get; set; }
+
+
     }
 }
