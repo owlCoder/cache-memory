2c454e3520fe203b4751ebf8226e00bdb36eec54
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-14 11:28:25 +0100

    Prepare models

--- Database/Servisi/IRegister.cs
+++ Database/Servisi/IRegister.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Database
+namespace Database.Servisi
 {
-    public class Class1
+    public class IRegister
     {
     }
 }
