093ff1232a888e34d4627a1e46bceae9e741f065
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-14 11:24:19 +0100

    Create DB

---
+++ Database/Modeli/Korisnik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Modeli
+{
+    public class Korisnik
+    {
+        public int Uid { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Adresa { get; set; }
+    }
+}
