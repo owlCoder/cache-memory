13d25a540b0a765368ef5d04c171b8322b753f46
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-17 23:13:59 +0100

    Implements models

---
+++ src/Cache Memory/Models/Brojilo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache_Memory.Models
+{
+    public class Brojilo
+    {
+        private int id;
+        private string naziv;
+
+        public Brojilo(int id, string naziv)
+        {
+            Id = id;
+
+            if (naziv == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (naziv.Trim().Equals(string.Empty))
+            {
+                throw new ArgumentException();
+            }
+
+            Naziv = naziv;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+    }
+}
