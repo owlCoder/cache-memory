4f6a64318049ac964f199b67f13c23c32510b7c4
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-18 21:45:00 +0100

    Brojilo: Implement find by id

--- src/Cache Memory/DataAccessObject/Implementations/Brojila.cs
+++ src/Cache Memory/DataAccessObject/Implementations/Brojila.cs
@@ -2,6 +2,7 @@
 using Cache_Memory.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace Cache_Memory.DataAccessObject.Implementations
 {
@@ -9,7 +10,31 @@ namespace Cache_Memory.DataAccessObject.Implementations
     {
         public int Count()
         {
-            throw new NotImplementedException();
+            int brojBrojila = 0;
+
+            // formiranje upita
+            string upit = "SELECT COUNT(*) FROM BROJILO";
+
+            using (IDbConnection konekcija = Connection.ConnectionPool.GetConnection())
+            {
+                konekcija.Open(); // otvaranje konekcije
+
+                using (IDbCommand komanda = konekcija.CreateCommand())
+                {
+                    komanda.CommandText = upit;
+                    komanda.Prepare();
+
+                    using (IDataReader reader = komanda.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            brojBrojila = Convert.ToInt32(komanda.ExecuteScalar());
+                        }
+                    }
+                }
+            }
+
+            return brojBrojila;
         }
 
         public int Delete(Brojilo entity)
@@ -29,7 +54,28 @@ namespace Cache_Memory.DataAccessObject.Implementations
 
         public bool ExistById(int id)
         {
-            throw new NotImplementedException();
+            using (IDbConnection konekcija = Connection.ConnectionPool.GetConnection())
+            {
+                konekcija.Open(); // otvaranje konekcije
+
+                return ExistsById(id, konekcija);
+            }
+        }
+
+        private bool ExistsById(int id, IDbConnection konekcija)
+        {
+            string query = "SELECT *FROM BROJILO WHERE brojiloId = :brojilo_id";
+
+            using (IDbCommand komanda = konekcija.CreateCommand())
+            {
+                komanda.CommandText = query;
+
+                Utils.ParameterUtil.AddParameter(komanda, "brojilo_id", DbType.Int32);
+                komanda.Prepare();
+                Utils.ParameterUtil.SetParameterValue(komanda, "brojilo_id", id);
+
+                return komanda.ExecuteScalar() != null;
+            }
         }
 
         public IEnumerable<Brojilo> FindAll()
@@ -39,7 +85,40 @@ namespace Cache_Memory.DataAccessObject.Implementations
 
         public IEnumerable<Brojilo> FindAllById(IEnumerable<int> ids)
         {
-            throw new NotImplementedException();
+            // lista brojila
+            List<Brojilo> listaBrojila = new List<Brojilo>();
+
+            // formiranje upita
+            string upit = "SELECT *FROM BROJILO";
+
+            using (IDbConnection konekcija = Connection.ConnectionPool.GetConnection())
+            {
+                konekcija.Open(); // otvaranje konekcije
+
+                using (IDbCommand komanda = konekcija.CreateCommand())
+                {
+                    komanda.CommandText = upit;
+                    komanda.Prepare();
+
+                    using (IDataReader reader = komanda.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // izdvanje podataka iz procitanog reda u tabeli
+                            int id = reader.GetInt32(0);
+                            string naziv = reader.GetString(1);
+
+                            // kreiranje objekta od iscitanih podataka
+                            Brojilo brojilo = new Brojilo(id, naziv);
+
+                            // dodavanje iscitanog brojila u listu
+                            listaBrojila.Add(brojilo);
+                        }
+                    }
+                }
+            }
+
+            return listaBrojila;
         }
 
         public Brojilo FindById(int id)
---
+++ src/Cache Memory/DataTransferObject/TrenutniKorisnik/PrijavljeniKorisnik.cs
@@ -0,0 +1,16 @@
+﻿using Cache_Memory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache_Memory.DataTransferObject.TrenutniKorisnik
+{
+    public class PrijavljeniKorisnik
+    {
+        private static Korisnik trenutniKorisnik = null;
+
+        public static Korisnik TrenutniKorisnik { get => trenutniKorisnik; set => trenutniKorisnik = value; }
+    }
+}
