da1be4ba655559230e41dbc746394551dfa98e1e
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-18 21:55:11 +0100

    Code clean up

--- src/Cache Memory/DataTransferObject/TrenutniKorisnik/PrijavljeniKorisnik.cs
+++ src/Cache Memory/DataTransferObject/TrenutniKorisnik/PrijavljeniKorisnik.cs
@@ -1,9 +1,4 @@
 ﻿using Cache_Memory.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cache_Memory.DataTransferObject.TrenutniKorisnik
 {
