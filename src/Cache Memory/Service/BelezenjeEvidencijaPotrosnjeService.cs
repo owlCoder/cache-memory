eb1123a808f827f6c88d8db6814e9be860b83e70
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-18 22:48:18 +0100

    Implement service

---
+++ src/Cache Memory/Service/BelezenjeEvidencijaPotrosnjeService.cs
@@ -0,0 +1,36 @@
+﻿using Cache_Memory.DataAccessObject.Implementations;
+using Cache_Memory.DataAccessObject.Interfaces;
+using Cache_Memory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache_Memory.Service
+{
+    public class BelezenjeEvidencijaPotrosnjeService
+    {
+        private static readonly IBelezenjeEvidencijaPotrosnje belezenjeEvidencije = new BelezenjeEvidencijaPotrosnje();
+
+        List<EvidencijaPotrosnje> GetEvidencijaAll()
+        {
+            return belezenjeEvidencije.FindAll().ToList();
+        }
+
+        List<EvidencijaPotrosnje> GetEvidencijaByMesec(int mesec)
+        {
+            return belezenjeEvidencije.FindByMesec(mesec).ToList();
+        }
+
+        List<EvidencijaPotrosnje> GetEvidencijaByUserId(int userId)
+        {
+            return belezenjeEvidencije.FindByUserId(userId).ToList();
+        }
+
+        List<EvidencijaPotrosnje> GetEvidencijaByGrad(string grad)
+        {
+            return belezenjeEvidencije.FindByGrad(grad).ToList();
+        }
+    }
+}
