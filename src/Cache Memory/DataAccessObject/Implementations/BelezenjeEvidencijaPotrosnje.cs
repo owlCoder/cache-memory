f4a5c63589bdd6f9a1d51f31ff64a0e40e7994cd
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-18 22:08:37 +0100

    Prepare new DAO

---
+++ src/Cache Memory/DataAccessObject/Implementations/BelezenjeEvidencijaPotrosnje.cs
@@ -0,0 +1,77 @@
+﻿using Cache_Memory.DataAccessObject.Interfaces;
+using Cache_Memory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache_Memory.DataAccessObject.Implementations
+{
+    public class BelezenjeEvidencijaPotrosnje : IBelezenjeEvidencijaPotrosnje
+    {
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(EvidencijaPotrosnje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EvidencijaPotrosnje FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EvidencijaPotrosnje> FindByMesec(int mesec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EvidencijaPotrosnje> FindByUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Save(EvidencijaPotrosnje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveAll(IEnumerable<EvidencijaPotrosnje> entities)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<EvidencijaPotrosnje> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EvidencijaPotrosnje> FindAllById(IEnumerable<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EvidencijaPotrosnje> FindByGrad(string grad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Cache Memory/DataAccessObject/Interfaces/IBelezenjeEvidencijaPotrosnje.cs
@@ -0,0 +1,22 @@
+﻿using Cache_Memory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache_Memory.DataAccessObject.Interfaces
+{
+    public interface IBelezenjeEvidencijaPotrosnje : ICRUD<EvidencijaPotrosnje, int>
+    {
+        // metoda koja pronalazi potrosnju za odredjeni mesec
+        IEnumerable<EvidencijaPotrosnje> FindByMesec(int mesec);
+
+        // metoda koja pronalazi potrosnje za odredjenog korisnika
+        IEnumerable<EvidencijaPotrosnje> FindByUserId(int id);
+
+        // metoda koja pronalazi potrosnje za odredjeni grad
+        IEnumerable<EvidencijaPotrosnje> FindByGrad(string grad);
+
+    }
+}
