ca01e06a19eabe5dbe6116656dc4555b8da00bb8
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2022-12-18 15:34:46 +0100

    New Schema and Tasks

--- src/Cache Memory/Connection/ConnectionParameters.cs
+++ src/Cache Memory/Connection/ConnectionParameters.cs
@@ -2,16 +2,16 @@
 {
     public class ConnectionParameters
     {
-#if DEBUG
-        // LOCAL DEVELOPEMENT
-        public static readonly string DATA_SOURCE = "//localhost:1521/xe";
-        public static readonly string USER_ID = "ers_db";
-        public static readonly string PASSWORD = "ers";
-#else
+        #if DEBUG
+                // LOCAL DEVELOPEMENT
+                public static readonly string DATA_SOURCE = "//localhost:1521/xe";
+                public static readonly string USER_ID = "ers_db";
+                public static readonly string PASSWORD = "ers";
+        #else
                 // AZURE DATABASE
                 public static readonly string DATA_SOURCE = "https://portal.azure.com/oracle/xe:1521/cache-memory5114";
                 public static readonly string USER_ID = "sysdba_azure";
                 public static readonly string PASSWORD = "43JK#d65260";
-#endif
+        #endif
     }
 }
