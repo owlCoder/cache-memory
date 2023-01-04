9d8330a979a259b05510a08f2677b5e6ab16883f
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2023-01-04 21:22:17 +0100

    Sprint 2

---
+++ src/Historical/Implementations/Historical.cs
@@ -0,0 +1,25 @@
+﻿using Common.Implementations;
+using Historical.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Historical.Implementations
+{
+    public class Historical : IHistorical
+    {
+        public IEnumerable<ModelData> GetAllDataFromDataBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ModelData> GetSelectedDataByCriteria(string criteriaName, string criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WriteModelDataToDataBase(ModelData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
