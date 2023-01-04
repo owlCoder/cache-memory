a01798e25b226790861cb4447fd2b2c8b3b7ff5e
BojanKuljic <bojankuljic16@gmail.com>
2023-01-04 21:52:42 +0100

    Kreiranje konekcije na DB

--- src/Common/Implementations/Connection.cs
+++ src/Common/Implementations/Connection.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.OracleClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,39 @@ namespace Common.Implementations
 {
     public class Connection : IConnection
     {
+        private static IDbConnection instance = null;
+
+        [Obsolete]
         public IDbConnection GetConnection()
         {
-            throw new NotImplementedException(); // BK
+            if (instance == null || instance.State == System.Data.ConnectionState.Closed)
+            {
+                OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
+
+                ocsb.UserID = ConnectionParams.USER_ID;
+                ocsb.Password = ConnectionParams.PASSWORD;
+                ocsb.DataSource = ConnectionParams.LOCAL_DATA_SOURCE;
+                ocsb.Pooling = true;
+                ocsb.MinPoolSize = 1;
+                ocsb.MaxPoolSize = 10;             
+
+                instance = new OracleConnection(ocsb.ConnectionString);                
+
+            }          
+           
+            return instance;
         }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Closing connection...");
+            if (instance != null)
+            {
+                instance.Close();
+                instance.Dispose();
+            }
+
+        }
+
     }
 }
