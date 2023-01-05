70c27fcdd0ff4c1bbe3bd9677c3d426f9e7093a9
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2023-01-05 18:40:37 +0100

    Prepare new impls

---
+++ src/Reader/Implementations/Reader.cs
@@ -0,0 +1,18 @@
+﻿using Common.Implementations;
+using Reader.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader.Implementations
+{
+    public class Reader : IReader
+    {
+        public List<ModelData> GetPodaciFromHistorical(string criteriaName, string criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Reader/Interfaces/IReader.cs
@@ -0,0 +1,14 @@
+﻿using Common.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader.Interfaces
+{
+    public interface IReader
+    {
+        List<ModelData> GetPodaciFromHistorical(string criteriaName, string criteria);
+    }
+}
