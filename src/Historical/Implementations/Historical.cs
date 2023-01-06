bfa9b29da97cb5f611fba5616bef08d261d6a8d0
Vuk <wookie.ognjanovic@gmail.com>
2023-01-06 17:57:46 +0100

    open connection

--- src/Historical/Implementations/Historical.cs
+++ src/Historical/Implementations/Historical.cs
@@ -2,6 +2,7 @@
 using Historical.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace Historical.Implementations
 {
@@ -9,8 +10,43 @@ namespace Historical.Implementations
     {
         public IEnumerable<ModelData> GetAllDataFromDataBase()
         {
-            throw new NotImplementedException();
+            string query = "select * from Potrosnja_Energije";
+            List<ModelData> dataList = new List<ModelData>();
+
+            using (IDbConnection connection = Connection.GetConnection())
+            {
+                connection.Open();
+                
+
+            }
         }
+
+        /*
+        public IEnumerable<Theatre> FindAll()
+        {
+            string query = "select id_th, name_th, address_th, website_th, place_id_pl from theatre";
+            List<Theatre> theatreList = new List<Theatre>();
+
+            using (IDbConnection connection = ConnectionUtil_Pooling.GetConnection())
+            {
+                connection.Open();
+                using (IDbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    command.Prepare();
+
+                    using (IDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Theatre theatre = new Theatre(reader.GetInt32(0), reader.GetString(1),
+                                reader.GetString(2), reader.GetString(3), reader.GetInt32(4));
+                            theatreList.Add(theatre);
+                        }
+                    }
+                }
+            }
+        */
 
         public IEnumerable<ModelData> GetSelectedDataByCriteria(string criteriaName, string criteria)
         {
