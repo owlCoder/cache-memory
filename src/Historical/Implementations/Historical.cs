74c31325f057a36a376ebab8c7a5bde60642261e
Vuk <wookie.ognjanovic@gmail.com>
2023-01-06 18:07:23 +0100

    GetAllData func

--- src/Historical/Implementations/Historical.cs
+++ src/Historical/Implementations/Historical.cs
@@ -10,18 +10,43 @@ namespace Historical.Implementations
     {
         public IEnumerable<ModelData> GetAllDataFromDataBase()
         {
-            string query = "select * from Potrosnja_Energije";
+            string query = "select * from POTROSNJA_ENERGIJE";
             List<ModelData> dataList = new List<ModelData>();
 
             using (IDbConnection connection = Connection.GetConnection())
             {
                 connection.Open();
-                
+                using (IDbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    command.Prepare();
 
+                    using (IDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ModelData data = new ModelData(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3),
+                                                                reader.GetString(4), reader.GetDecimal(5), reader.GetString(6));
+                            dataList.Add(data);
+                        }
+                    }
+                }
             }
         }
 
         /*
+         * public string UserID { get; set; }
+        public string Username { get; set; }
+        public string UserAddress { get; set; }
+        public string UserCity { get; set; }
+        public string BrojiloId { get; set; }
+        public decimal Potroseno { get; set; }
+        public string Mesec { get; set; }
+
+
+
+         * 
+         * 
         public IEnumerable<Theatre> FindAll()
         {
             string query = "select id_th, name_th, address_th, website_th, place_id_pl from theatre";
