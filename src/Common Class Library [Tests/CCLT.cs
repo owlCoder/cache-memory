0ae2290ce1e30742be166a7f532120f1c6c78f8b
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2023-01-15 16:59:27 +0100

    CCTL Tests init

---
+++ src/Common Class Library [Tests/CCLT.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Common_Class_Library_Tests
+{
+    [TestFixture]
+    public class CCLT
+    {
+        [Test]
+        [TestCase ()]
+        public void 
+    }
+}
