7778ac26e8465edc7193d8e2a003c10d81be1977
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-01-17 15:00:10 +0100

    test

--- src/Code Tests/CommonClassLibraryTests/CommonTests.cs
+++ src/Code Tests/CommonClassLibraryTests/CommonTests.cs
@@ -31,6 +31,9 @@ namespace CommonClassLibraryTests
             Assert.NotNull(modelData);
         }
 
+        [Test]
+        [TestCase(1, "uname", "addrr", "city", "SE-515", 12, "Januar")]
+        [TestCase(2, "uname", "addrr", "city", "SE-515", 12, "Februar")]
         public void TestModelDataParams(int userID, string username, string userAddress, string userCity, string brojiloId, decimal potroseno, string mesec)
         {
             ModelData modelData = new ModelData(userID, username, userAddress, userCity, brojiloId, potroseno, mesec);
