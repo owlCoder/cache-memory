e3c4ad08b810f89bc3a89c9c736f6c88175b152f
Vuk <wookie.ognjanovic@gmail.com>
2023-01-17 14:28:36 +0100

    historical tests setup

--- src/Code Tests/HistoricalTests/Class1.cs
+++ src/Code Tests/HistoricalTests/Class1.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HistoricalTests
 {
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        [TestCase()]
     }
 }
