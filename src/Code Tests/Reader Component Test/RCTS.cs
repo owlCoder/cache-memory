f7d5c7ed3821b4a38262354fa8ebc3abd8d50648
Dora Mitic <doramtc8@gmail.com>
2023-01-17 14:33:30 +0100

    Reader test class

---
+++ src/Code Tests/Reader Component Test/RCTS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader_Component_Test
+{
+    public class RCTS
+    {
+    }
+}
