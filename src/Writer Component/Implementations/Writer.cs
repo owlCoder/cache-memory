1171dd58342d6705af5a73e560346d4799e23f4a
BojanKuljic <bojankuljic16@gmail.com>
2023-01-17 14:37:28 +0100

    Implementacija prvog djela Writer.cs klase

--- src/Writer Component/Implementations/Writer.cs
+++ src/Writer Component/Implementations/Writer.cs
@@ -1,6 +1,7 @@
 ﻿using Common_Class_Library.Implementations;
 using DumpingBuffer_Component.Implementations;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Remoting;
 using Writer_Component.Interfaces;
 
@@ -8,6 +9,21 @@ namespace Writer_Component.Implementations
 {
     public class Writer : MarshalByRefObject, IWriter
     {
+
+        // Ne moze biti testirano - Security Sockets Polisa OS Windows
+        [ExcludeFromCodeCoverage]
+        public void DataPassThrough(ModelData data)
+        {
+            // Log Message
+            Console.WriteLine("[REQUEST] SAVE DATA TO BUFFER");
+
+            DumpingBuffer DumpingBufferINode = RemotingServices.Connect(typeof(DumpingBuffer), "tcp://localhost:8085/DumpingBuffer") as DumpingBuffer;
+            DumpingBufferINode.AddToQueue(data);
+
+            // Log Message
+            Console.WriteLine("[REQUEST] SAVE DATA TO BUFFER SUCCESS\n");
+        }
+
         public void DataPassThrough(ModelData data)
         {
             // Log Message
